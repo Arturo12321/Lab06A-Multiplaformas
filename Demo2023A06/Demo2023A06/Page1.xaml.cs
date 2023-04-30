fcf1434f6770560f160590855a0f822a2932d580
Arturo12321 <valerio.manrique@tecsup.edu.pe>
2023-04-29 19:43:20 -0500

    Agregar archivos de proyecto.

---
+++ Demo2023A06/Demo2023A06/Page1.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo2023A06
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            List<Persona> personas = new List<Persona>();
+            personas.Add(new Persona { Nombres = "Arturo", Apellidos = "Manrique", Edad = 21 });
+            personas.Add(new Persona { Nombres = "Aracely", Apellidos = "Manrique", Edad = 20 });
+            personas.Add(new Persona { Nombres = "Doris", Apellidos = "Pérez", Edad = 18 });
+            personas.Add(new Persona { Nombres = "Luna", Apellidos = "Pérez", Edad = 19 });
+
+            lvDemo.ItemsSource = personas;
+
+        }
+    }
+}
---
+++ Demo2023A06/Demo2023A06/Persona.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo2023A06
+{
+    public class Persona
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+    }
+}
